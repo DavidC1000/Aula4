a8f63eaa7007de099743596559ab5bc04faae59a
TÉCNICO EM INFORMÁTICA 2019.1 <36121742019.1@senacrjedu.local>
2019-08-06 11:32:21 -0300

    terceiro envio

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 /*javacript do site*/
 console.log("aplicação iniciada.");
+
+var img = document.getElementsByTagName('img')[0];
+    document.addEventListener(img, 'click', function(){
+    alert('clicou');
+}, false);
+
+var nav = document.getElementsByTagName('nav')[0];
+nav.addEventListener('click', function(){
+    alert('Navegando');
+}, false);
